c5e4e1cb26bb9bea7b855ee292b60fdd9ada8be5
Konstantin <89160200294@mail.ru>
2021-11-17 15:14:46 +0300

    Дописан тест для класса, работающего с файловой системой

--- src/test/java/com/websetvice/luxoft/webservice/service/EmployeeLoadServiceTest.java
+++ src/test/java/com/websetvice/luxoft/webservice/service/EmployeeLoadServiceTest.java
@@ -16,11 +16,10 @@ import static org.mockito.Mockito.*;
 public class EmployeeLoadServiceTest {
     private final EmployeeCrud employeeCrud = mock(EmployeeCrud.class);
     private final EmployeeLoadService service = new EmployeeLoadService(employeeCrud);
-//    private final EmployeeCrud employeeCrud = Mockito.verify(new EmployeeCrud(employeeRepository), Mockito.times(5000));
 
     @Test
-    public void readStream() throws LoadingException, InterruptedException {
-        InputStream is = EmployeeLoadServiceTest.class.getResourceAsStream("employee.xml");
+    public void readStreamTrue() throws LoadingException, InterruptedException {
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("employees.xml");
 
         CountDownLatch countDownLatch = new CountDownLatch(1);
         Mockito.doAnswer(answer -> {
@@ -31,6 +30,22 @@ public class EmployeeLoadServiceTest {
         service.readStream(is);
         countDownLatch.await(1, TimeUnit.MINUTES);
 
-        verify(employeeCrud, times(1)).add(new Employee("new name", 200, "division"));
+        verify(employeeCrud, times(1)).add(new Employee("Irina", 75, "very old division"));
+    }
+
+    @Test
+    public void readStreamFalse() throws LoadingException, InterruptedException  {
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("employees.xml");
+
+//        CountDownLatch countDownLatch = new CountDownLatch(1);
+//        Mockito.doAnswer(answer -> {
+//            countDownLatch.countDown();
+//            return answer.callRealMethod();
+//        }).when(employeeCrud).add(any(Employee.class));
+
+        service.readStream(is);
+//        countDownLatch.await(1, TimeUnit.MINUTES);
+
+        verify(employeeCrud, times(0)).add(new Employee("Ivan", 38, "new division"));
     }
 }
